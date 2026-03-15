e56324e510f5de488fa582b12a44ce58431339bd
gabzidevs <gabzidevs@gmail.com>
2026-03-15 13:53:17 +0800

    docs: add scaffolds-reference for multiple tech stacks

---
+++ scaffolds-reference/apps/web/eslint.config.js
@@ -0,0 +1,8 @@
+//  @ts-check
+
+import { tanstackConfig } from "@tanstack/eslint-config"
+
+/** @type {any} */
+const config = [...tanstackConfig]
+
+export default config
---
+++ scaffolds-reference/packages/ui/eslint.config.ts
@@ -0,0 +1,6 @@
+import { tanstackConfig } from "@tanstack/eslint-config"
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const config: any = [...tanstackConfig]
+
+export default config
