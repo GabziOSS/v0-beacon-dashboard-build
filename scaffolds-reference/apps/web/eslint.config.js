f9cb66218aacdd4062a17b759623e14c5075d547
gabzidevs <gabzidevs@gmail.com>
2026-03-15 14:26:23 +0800

    chore: remove orphaned scaffolds-reference directory

--- scaffolds-reference/apps/web/eslint.config.js
+++
@@ -1,8 +0,0 @@
-//  @ts-check
-
-import { tanstackConfig } from "@tanstack/eslint-config"
-
-/** @type {any} */
-const config = [...tanstackConfig]
-
-export default config
--- scaffolds-reference/packages/ui/eslint.config.ts
+++
@@ -1,6 +0,0 @@
-import { tanstackConfig } from "@tanstack/eslint-config"
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const config: any = [...tanstackConfig]
-
-export default config
